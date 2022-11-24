8876289a8728c5131eaebc64074b085f4f3c2661
Gabriel <bieloliveira0422@gmail.com>
2022-11-23 21:38:53 -0300

    Atulização 23/11/2022 - 20:38

--- src/app/pages/CadHorarios/index.js
+++ src/app/pages/CadHorarios/index.js
@@ -1,10 +1,46 @@
 import { Link } from "react-router-dom";
 export function CadHorario() {
-    return (
-      <div className="container">
-        <h1>CadHorarios</h1>
+  return (
+    <div className="container">
+      <div className="head">
         <Link to="/home">Voltar</Link>
+        <h1>CadHorarios</h1>
+      </div> 
+      <div>
+        <select name="cursos" id="cursos">
+          <option value="">Cursos</option>
+          <option value="">Design</option>
+          <option value="">Eletro</option>
+          <option value="">Elo</option>
+          <option value="">Info</option>
+          <option value="">Mab</option>
+          <option value="">Mec</option>
+          <option value="">Mov</option>
+          <option value="">Qui</option>
+        </select>
+        <select name="turmas" id="turmas">
+          <option value="">Turmas</option>
+        </select>
       </div>
-    );
-  }
-  
+      <div className="cadhorario">
+        <div className="turnos">
+          <h5>Manhã</h5>
+          <input type="text" placeholder="1° período" />
+          <input type="text" placeholder="2° período" />
+          <input type="text" placeholder="3° período" />
+          <input type="text" placeholder="4° período" />
+          <input type="text" placeholder="5° período" />
+        </div>
+        <div className="turnos">
+          <h5>Tarde</h5>
+          <input type="text" placeholder="1° período" />
+          <input type="text" placeholder="2° período" />
+          <input type="text" placeholder="3° período" />
+          <input type="text" placeholder="4° período" />
+          <input type="text" placeholder="5° período" />
+        </div>
+      </div>
+      <button id="save">Salvar</button>
+    </div>
+  );
+}
--- src/app/pages/CadTurmas/index.js
+++ src/app/pages/CadTurmas/index.js
@@ -1,9 +1,23 @@
 import { Link } from "react-router-dom";
 export function CadTurmas() {
+
+  function getTurma() {
+    var nameTurma=document.getElementById("nameTurma").value
+    alert("Turma "+nameTurma+" adicionada!")
+  }
+
+
+
     return (
       <div className="container">
-        <h1>CadTurmas</h1>
-        <Link to="/home">Voltar</Link>
+        <div className="head">
+          <Link to="/home">Voltar</Link>
+          <h1>CadTurmas</h1>
+        </div>
+        <div className="cadTurma">
+          <input type="text" id="nameTurma" placeholder="Digite a turma" />
+          <button id="addTurma" onClick={getTurma}>Adcionar Turma</button>
+        </div>
       </div>
     );
   }
